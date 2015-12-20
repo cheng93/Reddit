05737905efbc00bfaa1f2b6dcdbb8ae76ecaa780
cheng93 <derek.c@hotmail.co.uk>
2015-12-20 13:52:34 +0000

    program.cs to call Decrpyter

--- 235/Reddit235/Program.cs
+++ 235/Reddit235/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Reddit235
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Key");
+            var key = Console.ReadLine();
+            Console.WriteLine("Message");
+            var message = Console.ReadLine();
+
+            Console.WriteLine();
+
+            var decrypter = new Decrypter(key, message);
+            Console.WriteLine(decrypter.Decrypt());
+            Console.Read();
         }
     }
 }
