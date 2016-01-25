5f6317c46465a009e791b6d67098558909898ec5
cheng93 <derek.c@hotmail.co.uk>
2016-01-25 20:46:57 +0000

    Partition

---
+++ 250/Reddit250/Partition.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Reddit250
+{
+    public class Partition
+    {
+        public ICollection<int> Values { get; private set; }
+
+        public Partition(ICollection<int> values)
+        {
+            Values = values;
+        }
+
+        public static IEnumerable<Partition> GetPartitions(int number)
+        {
+            var output = new List<Partition>();
+            for (var i = number; i > 0; i--)
+            {
+                var list = new List<int> {i};
+                bool yielded = false;
+                var partitions = GetPartitions(number - i);
+                foreach (var partition in partitions)
+                {
+                    var partitionedList = new List<int>(list);
+                    partitionedList.AddRange(partition.Values);
+                    yielded = true;
+                    output.Add(new Partition(partitionedList));
+                }
+                if (!yielded)
+                {
+                    output.Add(new Partition(list));
+                }
+            }
+            return output.Distinct();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var partition = obj as Partition;
+            if (partition == null)
+            {
+                return false;
+            }
+            return Equals(partition);
+        }
+
+        public bool Equals(Partition partition)
+        {
+            if (partition == null)
+            {
+                return false;
+            }
+
+            if (Values.Count != partition.Values.Count)
+            {
+                return false;
+            }
+
+            return Values.Distinct().All(value => Values.Count(v => v == value) == partition.Values.Count(p => p == value));
+        }
+
+        public override int GetHashCode()
+        {
+            return (Values != null ? Values.Sum(v => v).GetHashCode() : 0);
+        }
+    }
+}
