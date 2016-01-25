c18135e7fa137fc599113fef1ef50c979b82c686
cheng93 <derek.c@hotmail.co.uk>
2016-01-25 20:47:06 +0000

    Partition Tests

---
+++ 250/Reddit250.UnitTest/PartitionTests/EqualsTest.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reddit250.UnitTest.PartitionTests
+{
+    [TestClass]
+    public class EqualsTest
+    {
+        [TestMethod]
+        public void Null_False()
+        {
+            var partition = new Partition(new List<int> { 1 });
+            object obj = null;
+            Assert.IsFalse(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void String_False()
+        {
+            var partition = new Partition(new List<int> { 1 });
+            var obj = "Hello World";
+            Assert.IsFalse(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void BothEmpty_True()
+        {
+            var partition = new Partition(new List<int>());
+            var obj = new Partition(new List<int>());
+            Assert.IsTrue(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void OneEmpty_False()
+        {
+            var partition = new Partition(new List<int>() { 1 });
+            var obj = new Partition(new List<int>());
+            Assert.IsFalse(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Simple_True()
+        {
+            var partition = new Partition(new List<int>() { 1 });
+            var obj = new Partition(new List<int>() { 1 });
+            Assert.IsTrue(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Complex_True()
+        {
+            var partition = new Partition(new List<int>() { 1, 2, 2, 3, 4 });
+            var obj = new Partition(new List<int>() { 1, 2, 2, 3, 4 });
+            Assert.IsTrue(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void DifferentCount_False()
+        {
+            var partition = new Partition(new List<int>() { 1 });
+            var obj = new Partition(new List<int>() { 1, 2 });
+            Assert.IsFalse(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Simple_SameElements_DifferentOrder_True()
+        {
+            var partition = new Partition(new List<int>() { 2, 1 });
+            var obj = new Partition(new List<int>() { 1, 2 });
+            Assert.IsTrue(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Complex_SameElements_DifferentOrder_True()
+        {
+            var partition = new Partition(new List<int>() { 2, 1, 2, 4, 3 });
+            var obj = new Partition(new List<int>() { 1, 2, 2, 3, 4 });
+            Assert.IsTrue(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Simple_SameCount_DifferentElements_False()
+        {
+            var partition = new Partition(new List<int>() { 1 });
+            var obj = new Partition(new List<int>() { 2 });
+            Assert.IsFalse(partition.Equals(obj));
+        }
+
+        [TestMethod]
+        public void Complex_SameCount_DifferentElements_False()
+        {
+            var partition = new Partition(new List<int>() { 1, 2, 2, 3, 4 });
+            var obj = new Partition(new List<int>() { 1, 2, 3, 4, 4 });
+            Assert.IsFalse(partition.Equals(obj));
+        }
+    }
+}
---
+++ 250/Reddit250.UnitTest/PartitionTests/GetPartitionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reddit250.UnitTest.PartitionTests
+{
+    [TestClass]
+    public class GetPartitionsTest
+    {
+        [TestMethod]
+        public void One()
+        {
+            var expected = new List<Partition>
+            {
+                new Partition(new List<int> {1})
+            };
+
+            var actual = Partition.GetPartitions(1).ToList();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            var expected = new List<Partition>
+            {
+                new Partition(new List<int> {2}),
+                new Partition(new List<int> {1,1})
+            };
+            var actual = Partition.GetPartitions(2).ToList();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            var expected = new List<Partition>
+            {
+                new Partition(new List<int> {3}),
+                new Partition(new List<int> {2,1}),
+                new Partition(new List<int> {1,1,1})
+            };
+            var actual = Partition.GetPartitions(3).ToList();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void Four()
+        {
+            var expected = new List<Partition>
+            {
+                new Partition(new List<int> {4}),
+                new Partition(new List<int> {3,1}),
+                new Partition(new List<int> {2,2}),
+                new Partition(new List<int> {2,1,1}),
+                new Partition(new List<int> {1,1,1,1})
+            };
+            var actual = Partition.GetPartitions(4).ToList();
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
