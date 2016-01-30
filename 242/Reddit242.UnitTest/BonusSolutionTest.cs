425c839cc87359940c4b19c3605913f0b534d0fb
cheng93 <derek.c@hotmail.co.uk>
2016-01-30 22:58:25 +0000

    BonusSolutionTest

---
+++ 242/Reddit242.UnitTest/BonusSolutionTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reddit242.UnitTest
+{
+    [TestClass]
+    public class BonusSolutionTest
+    {
+        private IReddit242 _reddit242 = new BonusSolution();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
