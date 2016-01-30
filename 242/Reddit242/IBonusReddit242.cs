5383749ecbd98d7b796de78ccc16fb38e495af8b
cheng93 <derek.c@hotmail.co.uk>
2016-01-30 23:19:18 +0000

    IBonusReddit242

---
+++ 242/Reddit242/IBonusReddit242.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Reddit242
+{
+    public interface IBonusReddit242 : IReddit242
+    {
+        IReadOnlyCollection<Show> Solve(IReadOnlyCollection<Show> shows, string preferredShowName);
+    }
+}
