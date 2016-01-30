56b9e75acca486e0134f7d82c1491e97e02a1367
cheng93 <derek.c@hotmail.co.uk>
2016-01-30 22:26:31 +0000

    ConsoleApp

---
+++ 242/Reddit242/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<string>()
+            {
+                "1530 1600",
+                "1605 1630",
+                "1645 1725",
+                "1700 1730",
+                "1700 1745",
+                "1705 1745",
+                "1720 1815",
+                "1725 1810"
+            };
+
+            foreach (var line in input)
+            {
+                
+            }
+        }
+    }
+}
