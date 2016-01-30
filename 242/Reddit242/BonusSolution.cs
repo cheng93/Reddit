5e0dbc3756c909f378f91f5dbfedad138bf93637
cheng93 <derek.c@hotmail.co.uk>
2016-01-30 23:19:40 +0000

    Implement IBonusReddit242

--- 242/Reddit242/BonusSolution.cs
+++ 242/Reddit242/BonusSolution.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Reddit242
 {
-    public class BonusSolution : Solution
+    public class BonusSolution : Solution, IBonusReddit242
     {
         public IReadOnlyCollection<Show> Solve(IReadOnlyCollection<Show> show, string preferredShowName)
         {
-            return null;
+            var preferredShow = show.FirstOrDefault(s => s.Name == preferredShowName);
+            if (preferredShow == null)
+            {
+                return Solve(show);
+            }
+
+            var output = new List<Show> { preferredShow };
+            output.AddRange(Solve(show.Where(s => s.Start >= preferredShow.End).ToList()));
+            output.AddRange(Solve(show.Where(s => s.End <= preferredShow.Start).ToList()));
+
+            return output;
         }
     }
 }
