c71387973be78f39ba07f413d27f12dbecd53123
cheng93 <derek.c@hotmail.co.uk>
2016-01-30 22:58:18 +0000

    BonusSolution

---
+++ 242/Reddit242/BonusSolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Reddit242
+{
+    public class BonusSolution : Solution
+    {
+        public IReadOnlyCollection<Show> Solve(IReadOnlyCollection<Show> show, string preferredShowName)
+        {
+            return null;
+        }
+    }
+}
