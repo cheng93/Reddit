afeaaaf6e2686b0324083926ab760d4783f26f36
cheng93 <derek.c@hotmail.co.uk>
2016-02-06 22:50:57 +0000

    StringUpdaterTest

---
+++ 252/Reddit252.UnitTest/StringUpdaterTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reddit252.UnitTest
+{
+    [TestClass]
+    public class StringUpdaterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
