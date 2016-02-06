c7d1295609cc980a9e0d5ec56a7ae0214582e1a6
cheng93 <derek.c@hotmail.co.uk>
2016-02-06 22:50:50 +0000

    StringUpdater

---
+++ 252/Reddit252/StringUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Reddit252.UnitTest")]
+namespace Reddit252
+{
+    internal class StringUpdater : IStringUpdater
+    {
+        public string UpdateWithPair(string input, char pair)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
