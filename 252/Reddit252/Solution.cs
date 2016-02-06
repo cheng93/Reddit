1b87fee678582a0cb9bb98e5fe89fa0c27c5b4dd
cheng93 <derek.c@hotmail.co.uk>
2016-02-06 21:17:12 +0000

    IPairGetter composite and solve()

--- 252/Reddit252/Solution.cs
+++ 252/Reddit252/Solution.cs
@@ -1,6 +1,26 @@
-﻿namespace Reddit252
+﻿using System;
+
+namespace Reddit252
 {
-    public class Class1
+    public class Solution
     {
+        private readonly IPairGetter _pairGetter;
+
+        public Solution(IPairGetter pairGetter)
+        {
+            _pairGetter = pairGetter;
+        }
+
+        public string Solve(string input)
+        {
+            var pair = _pairGetter.GetWidestLeftMostPair(input);
+            while (pair != null)
+            {
+                throw new NotImplementedException();
+                pair = _pairGetter.GetWidestLeftMostPair(input);
+            }
+
+            throw new NotImplementedException();
+        }
     }
 }
