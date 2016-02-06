1ad41b5dd72f4413ae6fe95d08bad62c459cf330
cheng93 <derek.c@hotmail.co.uk>
2016-02-06 23:48:27 +0000

    ConsoleAPp

---
+++ 252/Reddit252/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Reddit252;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = Solution.Get();
+            const string input = "ttvmswxjzdgzqxotby_lslonwqaipchgqdo_yz_fqdagixyrobdjtnl_jqzpptzfcdcjjcpjjnnvopmh";
+
+            Console.WriteLine(solution.Solve(input));
+            Console.ReadLine();
+        }
+    }
+}
