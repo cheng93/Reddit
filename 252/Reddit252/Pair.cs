d89978a44903925e22479d9dc7c5defb1098ed1a
cheng93 <derek.c@hotmail.co.uk>
2016-02-07 09:31:12 +0000

    Pair model

---
+++ 252/Reddit252/Pair.cs
@@ -0,0 +1,18 @@
+﻿namespace Reddit252
+{
+    public class Pair
+    {
+        public char Character { get; private set; }
+
+        public int StartIndex { get; private set; }
+
+        public int EndIndex { get; private set; }
+
+        public Pair(char character, int startIndex, int endIndex)
+        {
+            Character = character;
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+        }
+    }
+}
