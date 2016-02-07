3077c8cd0bec2becd214c3ca39dce21ff5f63102
cheng93 <derek.c@hotmail.co.uk>
2016-02-07 18:41:36 +0000

    Implement method

--- 252/Reddit252/NewPairGetter.cs
+++ 252/Reddit252/NewPairGetter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Reddit252.UnitTest")]
@@ -9,7 +10,42 @@ namespace Reddit252
     {
         public Pair GetWidestLeftMostPair(string input)
         {
-            throw new NotImplementedException();
+            Pair output = null;
+            var possiblePairStartIndex = new Dictionary<char, int>();
+            var previousIndex = new Dictionary<char, int>();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var character = input[i];
+
+                if (!possiblePairStartIndex.ContainsKey(character))
+                {
+                    possiblePairStartIndex.Add(character, i);
+
+                    if (previousIndex.ContainsKey(character))
+                    {
+                        previousIndex[character] = i;
+                    }
+                    else
+                    {
+                        previousIndex.Add(character, i);
+                    }
+                }
+                else
+                {
+                    var startIndex = possiblePairStartIndex[character];
+
+                    if (output == null || (output.EndIndex - output.StartIndex) < i - startIndex)
+                    {
+                        output = new Pair(character, startIndex, i);
+                    }
+
+                    possiblePairStartIndex = possiblePairStartIndex.Where(kvp => previousIndex[kvp.Key] >= previousIndex[character]).ToDictionary(kvp => kvp.Key, kvp => previousIndex[kvp.Key]);
+                    
+                    previousIndex[character] = i;
+                }
+            }
+            return output;
         }
     }
 }
