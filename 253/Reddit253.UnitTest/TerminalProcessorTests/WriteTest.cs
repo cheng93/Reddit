3f7f0d13d09a12b2723e3c0cbd301ddf2ed84207
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 15:55:33 +0000

    Rename tests

--- 253/Reddit253.UnitTest/TerminalProcessorTests/WriteTest.cs
+++ 253/Reddit253.UnitTest/TerminalProcessorTests/WriteTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reddit253.UnitTest.Mock;
+using Reddit253.UnitTest.Mock.MockTerminals;
 
 namespace Reddit253.UnitTest.TerminalProcessorTests
 {
@@ -10,8 +11,72 @@ namespace Reddit253.UnitTest.TerminalProcessorTests
             new MockTerminalClearer());
 
         [TestMethod]
-        public void TestMethod1()
+        public void Insert_Empty()
         {
+            var terminal = new MockTerminal();
+            _terminalProcessor.Process(terminal, "A");
+
+            var actual = terminal.GetValue(0, 0);
+            var expected = 'A';
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SetOverride()
+        {
+            var terminal = new MockTerminal();
+            _terminalProcessor.Process(terminal, "^o");
+
+            var actual = _terminalProcessor.IsInsertMode;
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void SetOverrideThanInsert()
+        {
+            var terminal = new MockTerminal();
+            _terminalProcessor.Process(terminal, "^o^i");
+
+            var actual = _terminalProcessor.IsInsertMode;
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Override_Empty()
+        {
+            var terminal = new MockTerminal();
+            _terminalProcessor.Process(terminal, "^oA");
+
+            var actual = terminal.GetValue(0, 0);
+            var expected = 'A';
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Insert_NonEmpty()
+        {
+            var terminal = new MockTerminal_NonEmpty();
+            _terminalProcessor.Process(terminal, "B");
+
+            var actual = terminal.GetValue(0, 0);
+            char? expected = 'B';
+            Assert.AreEqual(expected, actual);
+
+            actual = terminal.GetValue(0, 1);
+            expected = 'A';
+            Assert.AreEqual(expected, actual);
+
+            actual = terminal.GetValue(0, 2);
+            expected = ' ';
+            Assert.AreEqual(expected, actual);
+
+            actual = terminal.GetValue(0, 3);
+            Assert.IsNull(actual);
+
+            actual = terminal.GetValue(0, 4);
+            expected = 'C';
+            Assert.AreEqual(expected, actual);
         }
     }
 }
