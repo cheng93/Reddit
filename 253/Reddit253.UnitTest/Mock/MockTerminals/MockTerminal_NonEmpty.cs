b70a66c8b34a0c17e47942d427fb95d4085c603c
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 15:55:16 +0000

    Non Empty mock terminal

---
+++ 253/Reddit253.UnitTest/Mock/MockTerminals/MockTerminal_NonEmpty.cs
@@ -0,0 +1,21 @@
+﻿namespace Reddit253.UnitTest.Mock.MockTerminals
+{
+    internal class MockTerminal_NonEmpty : MockTerminal
+    {
+        public MockTerminal_NonEmpty()
+            : base(GetCharacters())
+        {
+            
+        }
+
+        public static char?[,] GetCharacters()
+        {
+            var characters = new char?[10, 10];
+            characters[0, 0] = 'A';
+            characters[0, 1] = ' ';
+            characters[0, 3] = 'C';
+
+            return characters;
+        }
+    }
+}
