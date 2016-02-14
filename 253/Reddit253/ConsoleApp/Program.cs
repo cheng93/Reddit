dde4e894b8146d67e041f32153f0da53dc891f68
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 16:51:00 +0000

    Remove fullstop in Console.App

---
+++ 253/Reddit253/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Reddit253;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var input = "";
+
+            var terminal = solution.Process(input);
+            var output = solution.Output(terminal);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
