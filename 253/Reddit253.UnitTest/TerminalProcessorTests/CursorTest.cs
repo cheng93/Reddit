847cd534bbb65cd430cd0204f9f00236a8b5901b
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 16:09:06 +0000

    CursorTest

---
+++ 253/Reddit253.UnitTest/TerminalProcessorTests/CursorTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reddit253.UnitTest.Mock;
+using Reddit253.UnitTest.Mock.MockTerminals;
+
+namespace Reddit253.UnitTest.TerminalProcessorTests
+{
+    [TestClass]
+    public class CursorTest
+    {
+        private readonly ITerminalProcessor _terminalProcessor = new TerminalProcessor(null, new MockTerminalClearer(),
+            new MockTerminalWriter(), new MockTerminalWriter());
+
+        [TestMethod]
+        public void MoveCursor()
+        {
+            var terminal = new MockTerminal();
+            _terminalProcessor.Process(terminal, "^44");
+
+            var actual = terminal.GetCursor();
+            var expected = new Point(4, 4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
