df723cc3c589d99b3e41e939484c84dce1f3d683
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 14:54:25 +0000

    Implement Process()

--- 253/Reddit253/TerminalProcessor.cs
+++ 253/Reddit253/TerminalProcessor.cs
@@ -29,11 +29,92 @@ namespace Reddit253
             _terminalClearer = terminalClearer ?? new TerminalClearer();
             _insertWriter = insertWriter ?? new InsertWriter();
             _overrideWriter = overrideWriter ?? new OverrideWriter();
+
+            IsInsertMode = true;
         }
 
         public void Process(ITerminal terminal, string input)
         {
-            throw new NotImplementedException();
+            input = input.Replace(Environment.NewLine, "");
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '^')
+                {
+                    if (i < input.Length - 1 && !char.IsDigit(input[i + 1]))
+                    {
+                        Process(terminal, input[i+1]);
+                        i++;
+                    }
+                    else if (i < input.Length - 2 && char.IsDigit(input[i + 1]) && char.IsDigit(input[i + 2]))
+                    {
+                        MoveCursor(terminal, input[i + 1], input[i + 2]);
+                        i++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    Write(terminal, input[i]);
+                }
+            }
+        }
+
+        private void Process(ITerminal terminal, char character)
+        {
+            switch (character)
+            {
+                case 'c':
+                    _terminalClearer.Clear(terminal);
+                    break;
+                case 'h':
+                    _cursorMover.Home(terminal);
+                    break;
+                case 'b':
+                    _cursorMover.Beginning(terminal);
+                    break;
+                case 'd':
+                    _cursorMover.Down(terminal);
+                    break;
+                case 'u':
+                    _cursorMover.Up(terminal);
+                    break;
+                case 'l':
+                    _cursorMover.Left(terminal);
+                    break;
+                case 'r':
+                    _cursorMover.Right(terminal);
+                    break;
+                case 'e':
+                    _terminalClearer.Erase(terminal);
+                    break;
+                case 'i':
+                    SetInsertMode();
+                    break;
+                case 'o':
+                    SetOverrideMode();
+                    break;
+                case '^':
+                    Write(terminal, character);
+                    break;
+                default:
+                    throw new ArgumentException("character not recognised", "character");
+            }
+        }
+
+        private void MoveCursor(ITerminal terminal, char rowChar, char columnChar)
+        {
+            int row, column;
+
+            if (!int.TryParse(rowChar.ToString(), out row))
+            {
+                throw new ArgumentException("rowChar not a digit", "rowChar");
+            }
+            if (!int.TryParse(columnChar.ToString(), out column))
+            {
+                throw new ArgumentException("columnChar not a digit", "columnChar");
+            }
+
+            _cursorMover.MoveTo(terminal, row, column);
         }
 
         private void SetInsertMode()
@@ -44,6 +125,12 @@ namespace Reddit253
         private void SetOverrideMode()
         {
             IsInsertMode = false;
+        }
+
+        private void Write(ITerminal terminal, char value)
+        {
+            Writer.Write(terminal, value);
+            _cursorMover.Right(terminal);
         }
     }
 }
