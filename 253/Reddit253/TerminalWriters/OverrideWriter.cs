28b369ef07e5a3e233701a9d3c6bd29ab5ed4122
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 14:12:51 +0000

    OverrideWriter

---
+++ 253/Reddit253/TerminalWriters/OverrideWriter.cs
@@ -0,0 +1,13 @@
+﻿namespace Reddit253.TerminalWriters
+{
+    internal class OverrideWriter : ITerminalWriter
+    {
+        public void Write(ITerminal terminal, char value)
+        {
+            var cursor = terminal.GetCursor();
+            var row = cursor.Y;
+            var column = cursor.X;
+            terminal.SetValue(row, column, value);
+        }
+    }
+}
