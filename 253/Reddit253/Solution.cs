306120669148b0d62933bdd26b6d31870dca5f10
cheng93 <derek.c@hotmail.co.uk>
2016-02-14 16:46:35 +0000

    Use terminalX in solution

--- 253/Reddit253/Solution.cs
+++ 253/Reddit253/Solution.cs
@@ -10,5 +10,18 @@
             _terminalProcessor = terminalProcessor ?? new TerminalProcessor();
             _terminalOutputter = terminalOutputter ?? new TerminalOutputter();
         }
+
+        public ITerminal Process(string input)
+        {
+            var terminal = new Terminal();
+            _terminalProcessor.Process(terminal, input);
+            return terminal;
+        }
+
+        public string Output(ITerminal terminal)
+        {
+            var output = _terminalOutputter.Output(terminal);
+            return output;
+        }
     }
 }
