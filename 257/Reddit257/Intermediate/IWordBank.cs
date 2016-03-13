002317a71fb2962d09e2e81f6683b4715ca92595
cheng93 <derek.c@hotmail.co.uk>
2016-03-13 13:02:03 +0000

    IWordBank

---
+++ 257/Reddit257/Intermediate/IWordBank.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Reddit257.Intermediate
+{
+    public interface IWordBank
+    {
+        IEnumerable<string> Get();
+    }
+}
---
+++ 257/Reddit257/Intermediate/WordBank.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Reddit257.Intermediate
+{
+    internal class WordBank : IWordBank
+    {
+        public IEnumerable<string> Get()
+        {
+            return File.ReadLines("../enable1.txt");
+        }
+    }
+}
