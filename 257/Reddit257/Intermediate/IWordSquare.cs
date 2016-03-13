e6e2972d0b18e740e91e2569d7b4bbd6566dd140
cheng93 <derek.c@hotmail.co.uk>
2016-03-13 15:51:33 +0000

    IWordSquare

---
+++ 257/Reddit257/Intermediate/IWordSquare.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Reddit257.Intermediate
+{
+    public interface IWordSquare : IList<string>
+    {
+        string GetWord(int column);
+    }
+}
---
+++ 257/Reddit257/Intermediate/WordSquare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Reddit257.Intermediate
+{
+    internal class WordSquare : List<string>, IWordSquare
+    {
+        public string GetWord(int column)
+        {
+            if (Count >= column)
+            {
+                return this[column];
+            }
+            return BuildPrefix(column);
+        }
+
+        private string BuildPrefix(int column)
+        {
+            var prefix = new StringBuilder();
+            foreach (var word in this)
+            {
+                prefix.Append(word[column]);
+            }
+            return prefix.ToString();
+        }
+    }
+}
